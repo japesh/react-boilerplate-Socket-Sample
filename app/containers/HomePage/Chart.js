09e847489578d3be33fc8f1d7bb72ee3e2027c62
japesh <japesh@daffodilsw.com>
2018-01-03 19:23:00 +0530

    chart view update

--- app/containers/HomePage/Chart.js
+++ app/containers/HomePage/Chart.js
@@ -1,9 +1,36 @@
+
 import React from "react";
 import DropDown from "../../components/DropDown";
-export default function Chart() {
-  return (
+import TradingView from "./TradeViewScript"
+export default class Chart extends React.Component{
+
+  render(){
+
+    return (
     <DropDown title={"chart"} open={true}>
-      <div>Chart Will Go here</div>
+      <ChartView/>
     </DropDown>
-  );
+  );}
+}
+class ChartView extends React.Component{
+  componentDidMount(){
+    new TradingView.widget({
+      "width": "100%",
+      "height": 500,
+      "symbol": "COINBASE:BTCUSD",
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "Dark",
+      "style": "1",
+      container_id:"widget-container",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "allow_symbol_change": true,
+      "hideideas": true
+    });
+  }
+  render(){
+    return <div id="widget-container"/>
+  }
 }
